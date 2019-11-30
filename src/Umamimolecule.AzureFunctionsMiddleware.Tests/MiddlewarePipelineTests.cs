dba2ccc023f9480150899a54460a84dcd4141c77
umamimolecule <umamimolecule@hotmail.com>
2019-11-30 22:03:37 +0800

    Remove unused namesapce imports

--- src/Umamimolecule.AzureFunctionsMiddleware.Tests/MiddlewarePipelineTests.cs
+++ src/Umamimolecule.AzureFunctionsMiddleware.Tests/MiddlewarePipelineTests.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ObjectPool;
